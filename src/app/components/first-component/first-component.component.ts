f5d7c5b05fd0600127ddf05bb3c85026f5e0f370
Matheus <matheusdiegoteixeira@gmail.com>
2023-08-02 18:28:35 -0300

    commit 02-08

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css'],
+})
+export class FirstComponentComponent {
+  name: string = 'Matheus';
+  age: number = 18;
+  job: string = 'Programador';
+  hobbies = ['academia', 'tocar instrumentos', 'escutar música', 'jogar', 'estudar']
+
+}
